55904c870985bd66d1fd1ff9449f77e418491311
stitchzdotnet <stitchzdotnet@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-07 03:55:22 +0000

    Added shortcode code
    git-svn-id: https://plugins.svn.wordpress.org/stitchz-social-login/tags/1.0.1@1084274 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ README.txt
@@ -0,0 +1,151 @@
+=== Stitchz Social Login ===
+Contributors: stitchzdotnet
+Link: http://www.stitchz.net/
+Tags: user authentication, social login, facebook, google, twitter, single sign-on, linkedin, social authentication, sso, vk, plugin, widget, yahoo, microsoft live, paypal, google plus, open id
+Requires at least: 3.9.2
+Tested up to: 3.9
+Stable tag: 1.0
+License: GPLv3
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+
+The Stitchz Social Login plugin adds the option to authenticate with one or
+more of the 20+ social identities providers supported by Stitchz.
+
+== Description ==
+
+The Stitchz Social Login plugin extends the standard WordPress user registration 
+and login experience by integrating social login features. With Stitchz Social 
+Login users can login with one or more supported social networks, including 
+Facebook, Twitter, Google, LinkedIn and more (20+).
+
+Stitchz Social Login provides a single, simple interface that maintains all
+your social identity provider information safely and securely (and encrypted
+while at rest). Using Stitchz saves time and eliminates custom code necessary to 
+integrate and manage multiple identity providers.
+
+Any user account can connect one or more social identities to their account
+and use them to login in with (before or after their account is created).
+
+= Supported Providers =
+- Facebook
+- Twitter
+- Google
+- Google Plus
+- LinkedIn
+- Tumblr
+- Box
+- Paypal
+- Yahoo
+- Dropbox
+- OpenID
+- Instagram
+- VK (Vkontakte)
+- Foursquare
+- Windows Live
+- SoundCloud
+- Discogs
+- Flickr
+- SalesForce.com
+- more...
+
+For additional details of the plugin, visit the project page:
+  http://www.stitchz.net/Wordpress
+
+To submit bug reports and feature suggestions, or to track changes:
+  http://stitchz.uservoice.com/forums/81839?lang=en
+
+
+== Installation ==
+= General Installation =
+Stitchz Social Login plugin can be downloaded and added as a plugin to any existing Wordpress installation. The plugin can be downloaded from: http://www.stitchz.net/Wordpress
+
+1. Login to Wordpress as an Administrator.
+
+2. Go to the Wordpress Admin counsel, then click "Plugins".
+
+3. Click "Add New".
+
+4. On the Add New page, click "Upload".
+
+5. Click "Browse" and navigate to where the stitchz_social_login.zip file was saved to and click open.
+
+6. Click "Install Now" to install the plugin.
+
+
+= Configuration =
+Before using Stitchz Social Login, you must setup an application at: <a href="https://login.stitchz.net/">https://login.stitchz.net/</a>. Once you application has been setup, continue with
+the steps below.
+
+1.  Login to your Wordpress site as an Administrator, and go to the Wordpress Admin counsel.
+
+2.  Click "Settings > Permalinks"
+
+3.  Change the default way WordPress handles web URLs. Under "Common Settings", select any option except "Default", and click "Save Changes". This will modify the .htaccess rewrite rules.
+
+4.  Next, click "Plugins".
+
+5.  Navigate to the Stitchz Social Login plugin in the plugins list and click "Activate".
+
+6.  Then, click "Configure".
+
+7.  Copy your App Url, ApiKey, AppSecret, and Return Url from your Stitch application into the appropriate fields in the Stitchz Social Login API Settings form. All fields with "(required)" are, you guessed it, required.
+
+    The "Return Url" is your Wordpress website's full web address plus the Stitchz Wordpress end point ('/stitchz_social_login/auth'), i.e. https://www.yourwebsiteaddress.com/stitchz_social_login/auth
+
+8.  Next, select the API Version to run. The "Standard Login" option is a basic authentication request used to only authenticate a user. The "OAuth 2 Login" option sends an OAuth 2.0 authenticated request to the Stitchz API and returns a valid OAuth 2.0 token. The token can be used later to make authorized requests to Stitchz.
+
+9.  Confirm your Stitchz Login API Settings before moving on to the next section.
+
+10. Click the "Sync Providers" button to synchronize your provider list configured in your Stitchz application with your Wordpress instance. If all settings are correct a sample login will display with all your configured and active identity providers. Check the README.txt file if any errors occur.
+
+11. Enable/disable any login forms where the Stitchz Social Login links are displayed. By default the links are visible on all available forms.
+
+12. The "Social Login Notes" field can contain up to 255 letters or numbers in plain text and will be displayed under the social login links.
+
+13. Click Save.
+
+== USAGE ==
+
+* To login with any identity provider, click the provider link on the login page. The browser will be redirected to the identity provider's login page followed by (typically) a permissions/scope confirmation page. After a successful login the browser will be redirected back to the Wordpress website. 
+
+* Any user account can connect one or more social identities to their account. The Connected Identities section on the user profile page lists all social identities associated with the user. Identities can be removed by clicking the "X" next to the provider name, or added by click the provider name. 
+
+* Stitchz Login Shortcode can be used on any page/post by using the following: [stitchz_social_login_shortcode]
+
+== REQUIREMENTS ==
+
+None.
+
+
+== Frequently Asked Questions ==
+= If the sample login links do not display on the Stitchz Login API Settings page, check the following: =
+- Check that the App Url, ApiKey and AppSecret are entered exactly as displayed in your Stitchz application page at: <a href="https://login.stitchz.net/">https://login.stitchz.net/</a>
+
+- Confirm your settings, click Save and try again. 
+
+- Go to your Stitchz application and confirm the Return Url matches your website address plus the required Stitchz end point in Wordpress, i.e. 'http://www.YourWebsiteAddress.com/stitchz_social_login/auth'
+
+
+== Changelog ==
+
+  = 1.0.1 =
+  * Added shortcode code
+
+  = 1.0 =
+  * 1st version
+
+== Upgrade Notice ==
+
+  = 1.0 =
+  * 1st version
+
+  = 1.0.1 =
+  * Added shortcode code
+
+== Screenshots ==
+1. **Stitchz Social Login Settings**: The Stitchz Social Login Settings Screen
+
+== CONTACT ==
+
+Current maintainer(s):
+* Stitchzdotnet (Ethan Peterson) - <a href="http://www.twitter.com/stitchzdotnet">@stichzdotnet</a> on Twitter
---
+++ tools/http-utilities.php
@@ -0,0 +1,355 @@
+<?php
+
+/**
+ * Send request to Stitchz for access token.
+ *
+ * @param string $dnsalias
+ *   The app url as provided by Stitchz.
+ * @param string $apikey
+ *   The unique client id provided by Stitchz.
+ * @param string $appsecret
+ *   The application secret provided by Stitchz.
+ *
+ * @return string|bool
+ *   A valid oauth 2 access token or FALSE if the call fails.
+ */
+function stitchz_social_login_get_access_token( $dnsalias, $apikey, $appsecret ) {
+
+  // Remove the trailing slash if found, then build the api end point url.
+  $dnsalias = rtrim( esc_url_raw( $dnsalias ), '/' ) . '/';
+  $url = $dnsalias . 'api/oauth2/Token';
+
+  // Check for a valid $url (api) and return an error if invalid.
+  if ( FALSE === stitchz_social_login_check_is_valid_api_url( $url ) ) {
+    return '<div class="messages error"><ul><li>' . __( 'Invalid App Url. Ensure the App Url is correct or contact your service provider.', 'stitchz_social_login' ) . '</li></ul></div>';
+  }
+
+  // Build a valid request body.
+  $data = 'client_id=' . urlencode( $apikey ) . '&client_secret=' . urlencode( $appsecret ) . '&grant_type=client_credentials&format=json';
+
+  $options = array(
+    'method' => 'POST',
+    'headers' => array(
+      'content-type' => 'application/x-www-form-urlencoded',
+      'accept' => 'application/json',
+    ),
+    'body' => $data,
+  );
+
+  // TODO: we could use CURL here or give a choice of default Wordpress vs. Curl.
+
+  // Send the POST request to the api end point.
+  $response = wp_remote_post( $url, $options );
+
+  // Check for a Wordpress error in the response.
+  if ( is_wp_error( $response ) ) {
+    if ( class_exists( 'WP_Exception' ) && $response instanceof WP_Exception ) {
+      throw $response;
+    }
+    else {
+      throw new Exception( $response->get_error_message(), (int) $response->get_error_code() );
+    }
+  }
+
+  // Check for a valid response.
+  if ( is_array( $response ) && $response['response']['code'] ) {
+    switch ( $response['response']['code'] ) {
+      case '200':
+        if ( ! empty( $response['body'] ) ) {
+		  $json_response = json_decode( $response['body'] );
+
+          // Get the Access Token out of the response.
+          $access_token = $json_response->access_token;
+          $expiry_time = $json_response->expires_in;
+
+          // Check if the token is there.
+          if ( ! empty( $access_token ) ) {
+            return $access_token;
+          }
+        }
+        break;
+
+      case '401':
+        stitchz_social_login_log_debug( sprintf( 'Authentication Request - %s response code was %s - %s', $url, $response['response']['code'], $response['response']['message'] ) );
+        return '<div class="messages warning"><ul><li>' . __( 'Unauthorized request. Double check your API Key and App Secret and try again.', 'stitchz_social_login' ) . '</li></ul></div>';
+
+      case '404':
+        stitchz_social_login_log_debug( sprintf( 'Authentication Request - %s response code was %s - %s', $url, $response['response']['code'], $response['response']['message'] ) );
+        return '<div class="messages warning"><ul><li>' . __( 'Not Found. Double check your relaying party URL and try again.', 'stitchz_social_login' ) . '</li></ul></div>';
+
+      case '500':
+        stitchz_social_login_log_debug( sprintf( 'Authentication Request - %s response code was %s - %s', $url, $response['response']['code'], $response['response']['message'] ) );
+        return '<div class="messages error"><ul><li>' . __( 'Internal Server Error. Ensure the service provider is available and contact your service provider.', 'stitchz_social_login' ) . '</li></ul></div>';
+    }
+  }
+  return FALSE;
+}
+
+/**
+ * Sends authorized HTTP request to Stitchz api.
+ *
+ * @param string $url
+ *   The url to send api request.
+ * @param string $access_token
+ *   A valid oauth access token.
+ *
+ * @return json|bool
+ *   A valid json object or FALSE if the call fails.
+ */
+function stitchz_social_login_get_api_call( $url, $access_token ) {
+
+  // Check for a valid $url (api) and return an error if invalid.
+  if ( FALSE === stitchz_social_login_check_is_valid_api_url( $url ) ) {
+	return '<div class="messages error"><ul><li>' . __( 'Invalid App Url. Ensure the App Url is correct or contact your service provider.', 'stitchz_social_login' ) . '</li></ul></div>';
+  }
+
+  // Check if the given $access_token is empty and use it to submit the request.
+  if ( ! empty( $access_token ) ) {
+    $options = array(
+      'method' => 'GET',
+      'headers' => array(
+        'authorization' => 'Bearer ' . $access_token,
+        'accept' => 'application/json',
+      ),
+    );
+
+    // TODO: we could use CURL here or give a choice of default WordPress vs. Curl.
+
+    // Send the GET request to the api end point.
+    $response = wp_remote_get( $url, $options );
+
+    // Check for a Wordpress error in the response.
+    if ( is_wp_error( $response ) ) {
+        if ( class_exists( 'WP_Exception' ) && $response instanceof WP_Exception ) {
+            throw $response;
+        }
+        else {
+            throw new Exception( $response->get_error_message(), (int) $response->get_error_code() );
+        }
+    }
+
+    // Check for a valid response.
+    if ( is_array( $response ) && $response['response']['code'] ) {
+      switch ($response['response']['code'] ) {
+        case '200':
+          if ( ! empty( $response['body'] ) ) {
+            $json_response = json_decode( $response['body'] );
+            return $json_response;
+          }
+          break;
+
+        case '401':
+          stitchz_social_login_log_debug( sprintf( 'API Request - %s response code was %s - %s', $url, $response['response']['code'], $response['response']['message'] ) );
+          return '<div class="messages warning"><ul><li>' . __( 'Unauthorized request. Double check your API Key and App Secret and try again.', 'stitchz_social_login' ) . '</li></ul></div>';
+
+        case '404':
+          stitchz_social_login_log_debug( sprintf( 'API Request - %s response code was %s - %s', $url, $response['response']['code'], $response['response']['message'] ) );
+          return '<div class="messages warning"><ul><li>' . __( 'Not Found. Double check your relaying party URL and try again.', 'stitchz_social_login' ) . '</li></ul></div>';
+
+        case '500':
+          stitchz_social_login_log_debug( sprintf( 'API Request - %s response code was %s - %s', $url, $response['response']['code'], $response['response']['message'] ) );
+          return '<div class="messages error"><ul><li>' . __( 'Internal Server Error. Ensure the service provider is available and contact your service provider.', 'stitchz_social_login' ) . '</li></ul></div>';
+      }
+    }
+  }
+  return FALSE;
+}
+
+/**
+ * Setup and send an HTTP request to Stitchz to get an authenticated user's social profile.
+ *
+ * @param string $dnsalias
+ *   App url as provided by Stitchz.
+ * @param string $appsecret
+ *   The application secret provided by Stitchz.
+ *
+ * @return json|bool
+ *   A json encoded response from Stitchz API or FALSE if the call fails.
+ */
+function stitchz_social_login_authorization_handler_prepare_and_send_v1_request( $dnsalias, $appsecret ) {
+
+  // A POST request is preferred
+  $token = ( ! isset( $_POST['token'] ) ? ( ! isset( $_GET['token'] ) ? ( ! isset( $_REQUEST['token'] ) ? 0 : $_REQUEST['token'] ) : $_GET['token'] ) : $_POST['token'] );
+
+  // Check if a valid token response was returned.
+  if ( $token !== 0 ) {
+
+	// Remove the trailing slash if found, then build the api end point url.
+	$url = rtrim( esc_url_raw( $dnsalias ), '/' ) . '/Authentication/v1/Auth';
+	$data = 'Token=' . urlencode( $token ) . '&AppSecret=' . urlencode( $appsecret );
+
+	// Check for a valid $url (api) and return an error if invalid.
+    if ( FALSE === stitchz_social_login_check_is_valid_api_ur( $url ) ) {
+	  return '<div class="messages error"><ul><li>' . __( 'Invalid App Url. Ensure the App Url is correct or contact your service provider.', 'stitchz_social_login' ) . '</li></ul></div>';
+    }
+
+	$options = array(
+	  'method' => 'POST',
+	  'headers' => array(
+		'content-type' => 'application/x-www-form-urlencoded',
+		'accept' => 'application/json',
+	  ),
+	  'body' => $data,
+	);
+
+	// TODO: we could use CURL here or give a choice of default Wordpress vs. Curl.
+
+	// Send the POST request to the api end point.
+	$response = wp_remote_post( $url, $options );
+
+	// Check for a Wordpress error in the response.
+	if ( is_wp_error( $response ) ) {
+	  if ( class_exists( 'WP_Exception' ) && $response instanceof WP_Exception ) {
+		throw $response;
+	  }
+	  else {
+		throw new Exception( $response->get_error_message(), (int) $response->get_error_code() );
+	  }
+	}
+
+	// Check for a valid response.
+	if ( is_array( $response ) && $response['response']['code'] ) {
+	  switch ( $response['response']['code'] ) {
+		case '200':
+		  if ( ! empty( $response['body'] ) ) {
+			$json_response = json_decode( $response['body'] );
+
+			return $json_response;
+		  }
+		  break;
+
+		case '400':
+		  stitchz_social_login_log_debug( sprintf( 'V1 Profile Request - %s response code was %s - %s', $url, $response['response']['code'], $response['response']['message'] ) );
+		  return '<div class="messages warning"><ul><li>' . __( 'Bad Request. Double check your API Key and App Secret and try again.', 'stitchz_social_login' ) . '</li></ul></div>';
+
+		case '401':
+		  stitchz_social_login_log_debug( sprintf( 'V1 Profile Request - %s response code was %s - %s', $url, $response['response']['code'], $response['response']['message'] ) );
+		  return '<div class="messages warning"><ul><li>' . __( 'Unauthorized request. Double check your API Key and App Secret and try again.', 'stitchz_social_login' ) . '</li></ul></div>';
+
+		case '404':
+		  stitchz_social_login_log_debug( sprintf( 'V1 Profile Request - %s response code was %s - %s', $url, $response['response']['code'], $response['response']['message'] ) );
+		  return '<div class="messages warning"><ul><li>' . __( 'Not Found. Double check your relaying party URL and try again.', 'stitchz_social_login' ) . '</li></ul></div>';
+
+		case '500':
+		  stitchz_social_login_log_debug( sprintf( 'V1 Profile Request - %s response code was %s - %s', $url, $response['response']['code'], $response['response']['message'] ) );
+		  return '<div class="messages error"><ul><li>' . __( 'Internal Server Error. Ensure the service provider is available and contact your service provider.', 'stitchz_social_login' ) . '</li></ul></div>';
+	  }
+	}
+  }
+  return FALSE;
+}
+
+/**
+ * Setup and send an HTTP request to Stitchz to get an authenticated user's social profile.
+ *
+ * @param string $dnsalias
+ *   App url as provided by Stitchz.
+ * @param string $apikey
+ *   Unique client id provided by Stitchz.
+ * @param string $appsecret
+ *   The application secret provided by Stitchz.
+ * @param string $redirecturi
+ *   Callback url where Stitchz will send authenticated response.
+ *
+ * @return json|bool
+ *   A json encoded response from Stitchz API or FALSE if the call fails.
+ */
+function stitchz_social_login_authorization_handler_prepare_and_send_v2_request( $dnsalias, $apikey, $appsecret, $redirecturi ) {
+
+  // A POST request is preferred
+  $token = ( ! isset( $_POST['token'] ) ? ( ! isset( $_GET['token'] ) ? ( ! isset( $_REQUEST['token'] ) ? 0 : $_REQUEST['token'] ) : $_GET['token'] ) : $_POST['token'] );
+
+  // Check if a valid token response was returned.
+  if ( $token !== 0 ) {
+	// Check that the return url ends with the proper end point url.
+	if ( FALSE === strpos( $redirecturi, '/stitchz_social_login/auth' ) ) {
+	  $redirecturi .= '/stitchz_social_login/auth';
+	}
+
+	// Remove the trailing slash if found, then build the api end point url.
+	$url = rtrim( esc_url_raw( $dnsalias ), '/' ) . '/Authentication/v2/Auth';
+	$version = 'v2';
+	$data = 'client_id=' . urlencode( $apikey ) . '&client_secret=' . urlencode( $appsecret ) . '&grant_type=authorization_code&redirect_uri=' . urlencode( $redirecturi ) . '&code=' . urlencode( $token ) . '&version=' . urlencode( $version ) . '&format=json';
+
+    // Check for a valid $url (api) and return an error if invalid.
+    if ( FALSE === stitchz_social_login_check_is_valid_api_url( $url ) ) {
+	  return '<div class="messages error"><ul><li>' . __(  'Invalid App Url. Ensure the App Url is correct or contact your service provider.', 'stitchz_social_login' ) . '</li></ul></div>';
+    }
+
+	$options = array(
+	  'method' => 'POST',
+	  'headers' => array(
+		'content-type' => 'application/x-www-form-urlencoded',
+		'accept' => 'application/json',
+	  ),
+	  'body' => $data,
+	);
+
+	// TODO: we could use CURL here or give a choice of default Wordpress vs. Curl.
+
+	// Send the POST request to the api end point.
+	$response = wp_remote_post( $url, $options );
+
+	// Check for a Wordpress error in the response.
+	if ( is_wp_error( $response ) ) {
+	  if ( class_exists( 'WP_Exception' ) && $response instanceof WP_Exception ) {
+		throw $response;
+	  }
+	  else {
+		throw new Exception( $response->get_error_message(), (int) $response->get_error_code() );
+	  }
+	}
+
+	// Check for a valid response.
+	if ( is_array( $response ) && $response['response']['code'] ) {
+	  switch ( $response['response']['code'] ) {
+		case '200':
+		  if ( ! empty( $response['body'] ) ) {
+			$json_response = json_decode( $response['body'] );
+
+			return $json_response;
+		  }
+		  break;
+
+		case '400':
+		  stitchz_social_login_log_debug( sprintf( 'V2 Profile Request - %s response code was %s - %s', $url, $response['response']['code'], $response['response']['message'] ) );
+		  return '<div class="messages warning"><ul><li>' . __( 'Bad Request. Double check your API Key and App Secret and try again.', 'stitchz_social_login' ) . '</li></ul></div>';
+
+		case '401':
+		  stitchz_social_login_log_debug( sprintf( 'V2 Profile Request - %s response code was %s - %s', $url, $response['response']['code'], $response['response']['message'] )  );
+		  return '<div class="messages warning"><ul><li>' . __( 'Unauthorized request. Double check your API Key and App Secret and try again.', 'stitchz_social_login' ) . '</li></ul></div>';
+
+		case '404':
+		  stitchz_social_login_log_debug( sprintf( 'V2 Profile Request - %s response code was %s - %s', $url, $response['response']['code'], $response['response']['message'] ) );
+		  return '<div class="messages warning"><ul><li>' . __( 'Not Found. Double check your relaying party URL and try again.', 'stitchz_social_login' ) . '</li></ul></div>';
+
+		case '500':
+		  stitchz_social_login_log_debug( sprintf( 'V2 Profile Request - %s response code was %s - %s', $url, $response['response']['code'], $response['response']['message'] ) );
+        return '<div class="messages error"><ul><li>' . __( 'Internal Server Error. Ensure the service provider is available and contact your service provider.', 'stitchz_social_login' ) . '</li></ul></div>';
+	  }
+	}
+  }
+  return FALSE;
+}
+
+/**
+ * Checks if the given Url is a valid stitchz.net dns alias.
+ *
+ * @param string $url
+ *   The Url to check.
+ *
+ * @return bool
+ *   True if the Url is valid and False if not.
+ */
+function stitchz_social_login_check_is_valid_api_url( $url ) {
+  $arr = parse_url( $url );
+  if ( isset( $arr['host'] ) ) {
+    $host = $arr['host'];
+
+    // Check that the API end point is valid.
+    if ( preg_match( '#^(?:[^.]+\.)*stitchz\.net$#i', $host, $arr ) ) {
+      return TRUE;
+    }
+  }
+  return FALSE;
+}
